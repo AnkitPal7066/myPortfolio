a2617b215750b11460aadcad13317a68a4c42bf1
Ankit Pal <ankitpal698@gmail.com>
2024-06-02 11:56:51 +0530

    first commit

--- src/comp/Reacte.js
+++ src/comp/Reacte.js
@@ -2,6 +2,7 @@ import React from 'react';
 import GeekFoods from '../Images/GeekFoods.png';
 import Food2Fork from '../Images/Food2Fork.png';
 import { Helmet } from 'react-helmet';
+import buybuddy from '../Images/buybuddy.png'
 
 export default function Reacte() {
   return (
@@ -26,6 +27,35 @@ export default function Reacte() {
                   <h2 className="title-font font-medium text-lg text-gray-300">Geek<span className="text-red-600">Foods</span></h2>
                 </a>
                 <p className="mb-4 text-gray-300">Explore this website to view a visual showcase of my work with React and Tailwind CSS. Note that it serves as a design portfolio and does not include functional features.</p>
+                <span className="inline-flex">
+                  <a className="text-gray-500">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                    </svg>
+                  </a>
+                  <a className="ml-2 text-gray-500">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                      <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                    </svg>
+                  </a>
+                  <a className="ml-2 text-gray-500">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                    </svg>
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-4 lg:w-1/4 md:w-1/2">
+            <div className="h-full flex flex-col items-center text-center">
+              <img alt="team" className="shadow-md shadow-white  flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src={buybuddy} />
+              <div className="w-full">
+                <a href="https://ankitpal7066.github.io/buybuddy/">
+                  <h2 className="title-font font-medium text-lg text-red-600">Buy<span className="text-gray-300">Buddy</span></h2>
+                </a>
+                <p className="mb-4 text-gray-300">Explore this shopping website to see my work with React and Tailwind CSS. Note that it demonstrates the design and functionality of a shopping cart and product navigation system.</p>
                 <span className="inline-flex">
                   <a className="text-gray-500">
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
